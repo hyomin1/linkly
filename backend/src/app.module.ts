fcff2187cb694cf044adf83412c15703f83bbfde
hyomin <leehm123456@gmail.com>
2025-06-02 00:41:42 +0900

    chore: CORS 설정 추가로 프론트 배포 도메인 허용

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { LinkModule } from './link/link.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LinkModule, PrismaModule],
+  imports: [
+    LinkModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
